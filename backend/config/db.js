59c0e48915876845bad4f87f786f912b0d026fe6
Mojgan360 <mojgan360@gmail.com>
2021-02-18 00:45:52 +0100

    mongoose setup

---
+++ backend/config/db.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose'
+
+const connectDB = async () => {
+  try {
+    const conn = mongoose.connectDB(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    })
+    console.log(`MongoDB Connected: ${conn.connection.host}`)
+  } catch (error) {
+    console.error(`Error: ${error.message}`)
+    process.exit(1)
+  }
+}
